63a13dbe4950c13ba46572c27d01f4a9b7cc788c
Emil Stjerneman <emil@stjerneman.com>
2015-05-09 05:23:40 +0200

    Working with sockets instead.

--- client/client.js
+++ client/client.js
@@ -1,11 +1,22 @@
+Meteor._debug = (function (super_meteor_debug) {
+    return function (error, info) {
+        if (!(info && _.has(info, 'msg')))
+            super_meteor_debug(error, info);
+    }
+})(Meteor._debug);
+
+Meteor.subscribe('clients');
+
+var Messages = new Mongo.Collection(null);
+
 Meteor.startup(function() {
-    if (Session.get("username") == undefined)  {
+    if (!isAuthed()) {
         vex.defaultOptions.className = 'vex-theme-os';
         vex.dialog.prompt({
             message: 'To start chat we need to know who you are',
             placeholder: 'What is your name?',
             callback: function(name) {
-                Session.set("username", name);
+                setUsername(name);
                 init();
             },
             showCloseButton: false,
@@ -21,13 +32,49 @@ Meteor.startup(function() {
     }
 });
 
+Streamy.on("__join__", function(data) {
+    var msg = data.username + " has joined.";
+    insertMessage(data.time, msg, null, "__join__");
+});
+
+Streamy.on('__leave__', function(data) {
+    var msg = data.username + " has left.";
+    insertMessage(data.time, msg, null, "__leave__");
+});
+
+Streamy.on("__message__", function(message) {
+    var client = Clients.findOne({
+        sid: message.__from
+    });
+
+    insertMessage(message.time, message.content, client, "__message__");
+});
+
 Template.messages.helpers({
-    messages: function(){
+    messages: function() {
         return Messages.find({}, {sort: {time: 1}});
     }
 });
 
-Template.message.rendered = function () {
+Template.message.helpers({
+    isMessage: function() {
+        return this.type === "__message__";
+    },
+    isJoin: function() {
+        return this.type === "__join__";
+    },
+    isLeave: function() {
+        return this.type === "__leave__";
+    },
+    isLocalMessage: function() {
+        return this.type === "__local__";
+    },
+    messageType: function() {
+        return "message-" + this.type.replace(/\_/g, "");
+    }
+});
+
+Template.message.rendered = function() {
     var $scroll_container = $("#chat-container");
     $scroll_container.scrollTop($scroll_container.prop("scrollHeight"));
 };
@@ -35,11 +82,21 @@ Template.message.rendered = function () {
 Template.message_form.events = {
     'submit form': function(event) {
         event.preventDefault();
-        var message = document.getElementById('message');
-        if (message.value) {
-            Meteor.call("addMessage", message.value, Session.get("username"));
-            message.value = '';
+
+        if (!isAuthed()) {
+            return;
         }
+
+        var $element = $("#message");
+        var content = $element.val();
+
+        Streamy.broadcast("__message__", {
+            time: TimeSync.serverTime(),
+            content: content,
+            type: "message"
+        });
+
+        $element.val('');
     }
 };
 
@@ -47,6 +104,32 @@ Template.registerHelper("formatTimestamp", function (timestamp) {
     return moment(new Date(timestamp)).format("HH:mm:ss");
 });
 
+function insertMessage(time, content, client, type) {
+    if (!isAuthed()) {
+        return;
+    }
+
+    Messages.insert({
+        time: time,
+        content: content,
+        username: (client && client.username) ? client.username : null,
+        type: type
+    });
+}
+
 function init() {
-    Meteor.subscribe("messages", TimeSync.serverTime());
+    $("#message").focus();
+    insertMessage(TimeSync.serverTime(), "YATC - Yet another tjatter client by BratAnon", null, "__local__");
+    insertMessage(TimeSync.serverTime(), "Welcome " + Session.get("username"), null, "__local__");
+}
+
+function setUsername(username) {
+    Session.set("username", username);
+    Streamy.emit('username_set', {
+        username: username
+    });
+}
+
+function isAuthed() {
+    return Session.get("username") != undefined;
 }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,45 @@
-Meteor.publish("messages", function(date) {
-    return Messages.find({time: {$gt: date}}, {sort: {time: 1}});
+Streamy.onConnect(function(socket) {
+    Clients.insert({
+        sid: socket.id
+    });
+});
+
+Streamy.onDisconnect(function(socket) {
+    var client = Clients.findOne({
+        sid: socket.id
+    });
+
+    Streamy.broadcast("__leave__", {
+        sid: client.sid,
+        time: Date.now(),
+        username: client.username
+    });
+
+    Clients.remove({
+        sid: client.sid
+    });
+});
+
+Streamy.on('username_set', function(data, socket) {
+    if (!data.username) {
+        throw new Meteor.Error('Empty username');
+    }
+
+    Clients.update({
+        'sid': socket.id
+    }, {
+        $set: { username: data.username }
+    });
+
+    Streamy.broadcast('__join__', {
+        sid: socket.id,
+        time: Date.now(),
+        username: data.username
+    });
+});
+
+Meteor.publish('clients', function() {
+    return Clients.find({
+        'username': { $ne: null }
+    });
 });
